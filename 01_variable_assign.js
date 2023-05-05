fc723691a789a075481c268fc377241c0825b925
MHMJ72 <mj.mhetre72@gmail.com>
2023-05-05 21:43:19 +0530

    Assign js

---
+++ 01_variable_assign.js
@@ -0,0 +1,30 @@
+
+var myFullNane; // Variable Declaration 
+console.log(myFullNane); // undefined 
+
+myFullNane = "MANOJ MHETRE.";
+console.log(myFullNane);
+
+myFullNane = "MANOJ VIJAY MHETRE.";
+console.log(myFullNane);
+
+var myLove;
+myLove="I love only JS"
+console.log(myLove);
+
+var firstName; var lastName; var age; var address; var pincode; var gender; var isMaried;
+firstName="MANOJ"; 
+lastName="MHETRE";
+age=27;
+address="1133, VAJRABAD PETH, SANGOLA.";
+pincode="413307";
+gender="MALE";
+isMaried="YES";
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(address);
+console.log(pincode);
+console.log(gender);
+console.log(isMaried);
