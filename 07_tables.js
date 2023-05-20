328284bf65f55887060219715a423e0b9c4b507a
MHMJ72 <mj.mhetre72@gmail.com>
2023-05-21 00:20:16 +0530

    tables complete

---
+++ 07_tables.js
@@ -0,0 +1,39 @@
+// WAP to get table for 5 ==> 5   10   15   20  25  30   ---- 50
+for (let index = 5; index <=50; index=index+5) { // index = 11
+    console.log(index); // 0 1  2 ... 10 
+}
+console.log("After loop");
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+for (let index = 17; index <=170; index=index+17){
+    console.log(index);
+    }
+
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+for (let index = 190; index >= 19; index=index-19) { // index = 11
+    console.log(index);
+}
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+
+for (let index = 8; index <=80; index=index+8){
+    console.log(index);
+    }
+
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
